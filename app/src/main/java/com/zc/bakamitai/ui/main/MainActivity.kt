29515a853373a5ff8e23827028c384448ce3dc93
Zahi Chemaly <chemalyzahi@hotmail.com>
2022-01-18 22:40:36 +0200

    Added settings screen

--- app/src/main/java/com/zc/bakamitai/ui/main/MainActivity.kt
+++ app/src/main/java/com/zc/bakamitai/ui/main/MainActivity.kt
@@ -1,6 +1,9 @@
 package com.zc.bakamitai.ui.main
 
+import android.content.Intent
 import android.os.Bundle
+import android.view.Menu
+import android.view.MenuItem
 import androidx.navigation.fragment.NavHostFragment
 import androidx.navigation.ui.AppBarConfiguration
 import androidx.navigation.ui.setupActionBarWithNavController
@@ -9,12 +12,14 @@ import com.google.android.material.bottomnavigation.BottomNavigationView
 import com.zc.bakamitai.R
 import com.zc.bakamitai.databinding.ActivityMainBinding
 import com.zc.bakamitai.ui.base.BaseActivity
+import com.zc.bakamitai.ui.settings.SettingsActivity
 
 class MainActivity : BaseActivity<ActivityMainBinding>() {
     override fun getViewBinding() = ActivityMainBinding.inflate(layoutInflater)
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        setSupportActionBar(findViewById(R.id.toolbar))
         val navView: BottomNavigationView = binding.navView
         val navHostFragment =
             supportFragmentManager.findFragmentById(R.id.nav_host_fragment_activity_main) as NavHostFragment
@@ -26,5 +31,21 @@ class MainActivity : BaseActivity<ActivityMainBinding>() {
         )
         setupActionBarWithNavController(navController, appBarConfiguration)
         navView.setupWithNavController(navController)
+    }
+
+    override fun onCreateOptionsMenu(menu: Menu?): Boolean {
+        menuInflater.inflate(R.menu.action_bar_menu, menu)
+        return true
+    }
+
+    override fun onOptionsItemSelected(item: MenuItem): Boolean {
+        return when (item.itemId) {
+            R.id.action_settings -> {
+                val intent = Intent(this, SettingsActivity::class.java)
+                startActivity(intent)
+                true
+            }
+            else -> super.onOptionsItemSelected(item)
+        }
     }
 }
